d37603866999e9616080cce5c2d804eda99e1f9c
Sascha <sascha.fh.kubisch@gmail.com>
2023-04-19 18:36:18 +0200

    Debug App.js

--- docs/todos.md
+++ docs/todos.md
@@ -4,10 +4,10 @@
 
 ### This is to set up contracts and bridge carbon credits 
 x connect wallet debug 
-- whitelist treasury contract
-- mockBCT (Celo) on treasury contract
+x whitelist treasury contract
+- Testnet BCT (matic) on wrapper contract (owned by treasury?)
 
-- react useState -> treasury whitelist account -> wrap + bridge  frontend
+x react useState -> treasury whitelist account -> wrap + bridge  frontend
 - wrap & bridge mockBCT to erc1155 TCO2-001
 - offer TCO2-001
 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,9 +6,11 @@ import SellCarbonCredits from './components/Customer/SellCarbonCredits';
 import OffsetCarbonFootprint from './components/Customer/OffsetCarbonFootprint';
 import WrapCarbonCredits from './components/Treasury/WrapCarbonCredits';
 import BridgeCarbonCredits from './components/Treasury/BridgeCarbonCredits';
+import { treasury }from './utils/contracts' ;
+import tronWeb from "./utils/tronWeb";
 
-import tronWeb from './utils/tronWeb';
 
+const { address: treasuryAddress } = treasury;
 
 function App() {
   const [connected, setConnected] = useState(false);
@@ -20,18 +22,35 @@ function App() {
       if (window.tronWeb && window.tronWeb.ready) {
         setAddress(window.tronWeb.defaultAddress.base58);
         setConnected(true);
+
+        const isTreasury = await checkIfTreasury(window.tronWeb.defaultAddress.base58);
+        if (isTreasury) {
+          setUserType('treasury');
+        } else {
+          setUserType('customer');
+        }
       } else {
         setTimeout(connectTronLink, 500);
       }
     };
     connectTronLink();
   }, []);
-  
-  // Replace this with the actual function to check if the address is a treasury.
+
+  // Replace this with the actual function to check if the address is owner of the treasury contract.
   const checkIfTreasury = async (address) => {
-    // TODO: Check if the address is a treasury
-    return true;
+    if (address && window.tronWeb && window.tronWeb.ready) {
+      try {
+        const contractInstance = await window.tronWeb.contract(treasury.abi, treasuryAddress);
+        const ownerAddress = await contractInstance.methods.owner().call();
+        const ownerAddressBase58 = tronWeb.address.fromHex(ownerAddress);
+        return ownerAddressBase58 === address;
+      } catch (error) {
+        console.error('Error checking if the address is owner of the treasury contract:', error);
+      }
+    }
+    return false;
   };
+  
 
   return (
     <div className="App">
